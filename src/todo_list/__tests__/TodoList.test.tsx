7775d9251d6b9196834aac2e3d9d6bcab59fac77
NyanShen <3354509516@qq.com>
2019-08-15 21:02:08 +0800

    mosk state

--- src/todo_list/__tests__/TodoList.test.tsx
+++ src/todo_list/__tests__/TodoList.test.tsx
@@ -2,7 +2,21 @@ import * as React from "react";
 import {mount} from "enzyme";
 import {render} from "@testing-library/react";
 import TodoList from "../TodoList";
+import UndoList from "../UndoList";
 
-it("test todo list", () => {
-    const wrapper = render(<TodoList />)
+type MockImplementation = [any, React.Dispatch<any>];
+
+describe(" TodoList", () => {
+    const setState = jest.fn();
+    const stateSpy = jest.spyOn(React, "useState");
+    stateSpy.mockImplementationOnce(function (): MockImplementation{
+        return [[{
+            status: "div",
+            value: "test value Nyan"
+        }], setState]
+    })
+    it("test todo list", () => {
+        const wrapper = mount(<TodoList />)
+        console.log(wrapper.find(UndoList).props().list)
+    })
 })
