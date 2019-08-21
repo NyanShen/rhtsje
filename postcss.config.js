fd6a8714570c8705a298816a973235e63ad11d3f
NyanShen <3354509516@qq.com>
2019-08-21 20:58:46 +0800

    mobile adapter

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,21 @@
 const autoprefixer = require("autoprefixer");
+const pr2rem = require("postcss-plugin-pr2rem");
+const pr2remConfig = {
+  // 设计图为1242px， 一份root对应着rootWidth/100=12.42px
+  rootValue: 12.42,
+  // 基本单位， 前面设置了1vw
+  unitPrecision: 1,
+  propWhiteList: [],
+  propBlackList: ["font-size"],
+  selectorBlackList: [],
+  ignoreIdentifier: "00",
+  replace: true,
+  mediaQuery: false,
+  minPixelValue: 0
+}
 module.exports = {
-    plugins: [autoprefixer(["IE 10"])]
-  }
+  plugins: [
+    autoprefixer(["IE 10"]),
+    pr2rem(pr2remConfig)
+  ]
+}
