3cd9c75d45d146d8acc4dcfc9bb559e8f69c79c9
NyanShen <3354509516@qq.com>
2019-09-14 11:20:23 +0800

    form tabs

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -4,7 +4,16 @@ import "./index.scss";
 import SVGIcon from "../../components/svg-icon";
 
 const Login = () => {
+    const [loginWay, setloginWay] = useState<String>("pass");
     const [showPass, setShowPassword] = useState<Boolean>(false);
+
+    const switchLoginWay = (way: String) => {
+        setloginWay(way);
+    }
+    const getTabClass = (way: String) => {
+        let basicClass = "sym-form-tab";
+        return loginWay === way ? basicClass : basicClass + " sym-form-tab-active"
+    }
     const switchShowPassword = () => {
         setShowPassword(showPass => !showPass);
     }
@@ -12,8 +21,15 @@ const Login = () => {
         <div className="sym-login-content">
             <div className="sym-login-inner">
                 <form className="sym-login-form">
+                    <div className="sym-form-tabs">
+                        <div className={getTabClass("code")} onClick={() => switchLoginWay("pass")}>免密码登录</div>
+                        <div className={getTabClass("pass")} onClick={() => switchLoginWay("code")}>密码登录</div>
+                    </div>
                     <div className="sym-account">
-                        login form
+                        <div className="sym-input-wrapper">
+                            <input type="text" placeholder="手机号或邮箱" />
+                        </div>
+                        <div className="sym-input-error-mask">请输入手机号或邮箱</div>
                     </div>
                     <div className="sym-password">
                         <div className="sym-input-wrapper">
